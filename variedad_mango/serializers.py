38b4e0ab9f210c993ea706aff01c3c33204a2e60
agl5702 <tenshidesu12345@gmail.com>
2024-07-05 08:06:27 -0500

    add host

--- variedad_mango/serializers.py
+++ variedad_mango/serializers.py
@@ -1,2 +1,16 @@
 from rest_framework import serializers
-from variedad_mango.models import VariedadMango
+from variedad_mango.models import VariedadMango,AptitudSuelo
+
+
+class VariedadMangoSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = VariedadMango
+        fields = '__all__'
+        
+class AptitudSueloSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = AptitudSuelo
+        fields= '__all__'
+
--- variedad_mango/views.py
+++ variedad_mango/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from variedad_mango.models import VariedadMango,AptitudSuelo
+from variedad_mango.serializers import VariedadMangoSerializer,AptitudSueloSerializer
 # Create your views here.
+
+class VariedadMangoView(viewsets.ModelViewSet):
+    
+    serializer_class = VariedadMangoSerializer
+    queryset = VariedadMango.objects.all()
+
+class AptitudSueloView(viewsets.ModelViewSet):
+    
+    serializer_class = AptitudSueloSerializer
+    queryset = AptitudSuelo.objects.all()
