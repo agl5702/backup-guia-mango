f73231abee9fa625d3712c64a18f0c9aaa0f10cd
kerenArias-2001 <kerenesther2001@gmail.com>
2024-07-09 17:13:04 -0500

    add build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,15 @@
 set -o errexit
 
 pip install -r requirements.txt
+python makemigrations users
+python makemigrations tipo_siembra
+python makemigrations herramientas_agricultura_precision
+python makemigrations hectareas
+python makemigrations suelo
+python makemigrations variedad_mango
+python makemigrations cultivo
+python makemigrations analisis_foliar
+python makemigrations informes
+python makemigrations tablas_estadisticas
 python manage.py migrate
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin@admin.com', 'admin', 'admin')"
