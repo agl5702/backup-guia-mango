51291695d92c6d9c2e6caaf64534c9810de8b298
kerenArias-2001 <kerenesther2001@gmail.com>
2024-07-09 17:03:46 -0500

    add build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,5 @@
 set -o errexit
 
 pip install -r requirements.txt
-
-python manage.py collectstatic --no-input 
 python manage.py migrate
-
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin@admin.com', 'admin', 'admin')"
