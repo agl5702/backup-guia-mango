84685afb4ade459f3164b55b78e97c7fab29bb56
agl5702 <tenshidesu12345@gmail.com>
2024-07-12 07:24:04 -0500

    add

--- analisis_foliar/models.py
+++ analisis_foliar/models.py
@@ -11,7 +11,7 @@ class AnalisisFoliar(models.Model):
     #eliminar choices
     nutrient = models.CharField(max_length=45)
     soil_analysis = models.ForeignKey(AnalisisSuelo, on_delete=models.CASCADE)
-    level= models.CharField('Nivel', choices=level_choices,max_length=10)
+    level= models.CharField('Nivel', choices=level_choices,max_length=10,default=0)
     class Meta:
         db_table = 'Analisis_Foliar'
         verbose_name_plural = 'Análisis Foliar'
