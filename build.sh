febbf6dd0cbdaadbc2ae37e54ca30e2642ab1da5
agl5702 <137901683+agl5702@users.noreply.github.com>
2024-07-17 07:23:32 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,4 +21,4 @@ python manage.py makemigrations tablas_estadisticas
 python manage.py migrate
 
 # Crear un superusuario (si es necesario)
-#python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin@admin.com', 'admin', 'admin')"
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin@admin.com', 'admin', 'admin')"
