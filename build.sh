5fbfc474b5d7c2365995c029b23babe31e69f025
agl5702 <advasquez41@misena.edu.co>
2024-12-02 10:20:02 -0500

    fixtures

--- build.sh
+++ build.sh
@@ -27,6 +27,19 @@ python manage.py makemigrations plagas
 # Aplicar todas las migraciones pendientes a la base de datos
 python manage.py migrate
 
+python manage.py loaddata user.json
+python manage.py loaddata tipo_siembra.json
+python manage.py loaddata variedad.json
+python manage.py loaddata herramientas.json
+python manage.py loaddata fertilidad_suelo.json
+python manage.py loaddata ph.json
+python manage.py loaddata textura_suelo.json
+python manage.py loaddata tipo_suelo.json
+python manage.py loaddata mercado.json
+python manage.py loaddata hectareas.json
+
+
+
 /opt/render/project/src/demon.sh &
 
 
